91f5b0a044d714603eb33cbf7ad405f100cac133
Maria Sapovalova <maria_sap@Marias-MacBook-Pro.local>
2024-10-09 18:58:59 +0100

    created Team class

--- src/Team.java
+++ src/Team.java
@@ -1,5 +1,31 @@
 package src;
+import java.util.ArrayList;
 
 public class Team {
-    
+    private boolean vulnerable;
+    private int trickCount;
+    private ArrayList<Player> teamPlayers;
+
+    public Team(boolean vulnerable, int trickCount){
+        this.vulnerable = vulnerable;
+        this.trickCount = trickCount;
+        teamPlayers = new ArrayList<>();
+    }
+
+    public void setTeam(Player player1, Player player2){
+        teamPlayers.add(player1);
+        teamPlayers.add(player2);
+    }
+
+    public boolean getVulnerabilityStatus(){
+        return vulnerable;
+    }
+
+    public int getTrickCount(){
+        return trickCount;
+    }
+
+    public ArrayList<Player> getTeamPlayers(){
+        return teamPlayers;
+    }
 }
