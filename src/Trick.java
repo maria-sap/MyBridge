3b0d5117c9eab6ad05787b2b8cd96c7a61911c43
Maria Sapovalova <maria_sap@Marias-MacBook-Pro.local>
2024-10-09 13:45:50 +0100

    wrote basic Trick class

---
+++ src/Trick.java
@@ -0,0 +1,31 @@
+package src;
+import java.util.ArrayList;
+
+public class Trick {
+    private Card startCard;
+    private String startSuit;
+    private ArrayList<Card> trickCards;
+
+    public Trick(Card startCard){
+        this.startCard = startCard;
+        trickCards = new ArrayList<>();
+    }
+
+    // setters
+    // function to add card to array
+    public void placeCard(Card card){
+        trickCards.add(card);
+    }
+
+    // sets first card 
+    public void retrieveLeadSuit(){
+        startCard = trickCards.get(0);
+        startSuit = startCard.getSuit();
+    }
+
+    // getters
+    // get start suit
+    public String getStartSuit(){
+        return startSuit;
+    }
+}
