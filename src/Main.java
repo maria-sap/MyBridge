a4f3fdac73cf3772d9ca13386445985d4d3c36c5
Maria Sapovalova <maria_sap@Marias-MacBook-Pro.local>
2024-10-12 17:34:31 +0100

    team1 & team2 init

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Player> players = new ArrayList<>();
         Deck deck = new Deck();
-        Team team1;
-        Team team2;
+        Team team1 = new Team();
+        Team team2 = new Team();
 
         // creating temporary players
         players.add(new Player(new Hand(), 1,  0, "None"));
@@ -15,7 +15,18 @@ public class Main {
         players.add(new Player(new Hand(), 3, 0, "None"));
         players.add(new Player(new Hand(), 4, 0, "None"));
         
-        //team1.setTeam(players.get(0), players.get(2));
+        team1.setTeam(players.get(0), players.get(2));
+        team2.setTeam(players.get(1), players.get(3));
+
+        System.out.println("team1");
+        for (Player player : team1.getTeamPlayers()){
+            System.out.println(player);
+        }
+        System.out.println("team2");
+        for (Player player : team2.getTeamPlayers()){
+            System.out.println(player);
+        }
+
         /* printing the shuffled deck */
         /*
         int i = 1;
