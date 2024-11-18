cb6b76455c04a57b0c2173c40d53b95c32f74aaa
Maria Sapovalova <maria_sap@Marias-MacBook-Pro.local>
2024-11-18 15:26:29 +0000

    made trickCount public

--- src/Team.java
+++ src/Team.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Team {
     private boolean vulnerable;
-    private int trickCount;
+    public int trickCount;
     private ArrayList<Player> players;
 
     public Team() {
